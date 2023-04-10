0544080466a23ad729e0468ab3d57cc004d7638e
Sebastian Sanchez <110407838+ssanchez-sonatype@users.noreply.github.com>
2023-04-10 16:21:52 -0300

    Update x.sh

--- .lift/x.sh
+++ .lift/x.sh
@@ -26,33 +26,37 @@ function run() {
 }
 
 function finalize() {
-        date >> /tmp/x.sh.finalize.log
-        echo "Args: $*" >> /tmp/x.sh.finalize.log                
-        printf "Stdin: " >> /tmp/x.sh.finalize.log
-        cat <&0 >> /tmp/x.sh.finalize.log
-        echo "" >> /tmp/x.sh.finalize.log        
+    {
+        date
+        echo "Args: $*"
+        printf "Stdin: "
+        cat <&0
+        echo ""
         local pr_branch
-        pr_branch="v3-tool-run-and-finish-$(date '+%F_%0H-%0M-%0S_%N')"    
-        echo "Checking out ${pr_branch} locally" >> /tmp/x.sh.finalize.log
-        git checkout -b "$pr_branch" &> /dev/null       
-        echo "" >> /tmp/x.sh.finalize.log
+        pr_branch="v3-tool-run-and-finish-$(date '+%F_%0H-%0M-%0S_%N')"
+        echo "Checking out ${pr_branch} locally"
+        git checkout -b "$pr_branch" &> /dev/null
+        echo ""
         cat <<EOF
-{ "toolNotes" : [
-    { "type" : "type during run",
-      "message" : "run message",
-      "file" : "a.c",
-      "line" : 3,
-      "noteId" : "noteid"
+{
+  "toolNotes": [
+    {
+      "type": "type during run",
+      "message": "run message",
+      "file": "a.c",
+      "line": 3,
+      "noteId": "noteid"
     }
-  ]
-, "pullRequest" : {
-      "title": "x.sh PR",
-      "body": "Bug fixes and performance improvements",
-      "target_branch": null,
-      "source_commit": null
+  ],
+  "pullRequest": {
+    "title": "x.sh PR",
+    "body": "Bug fixes and performance improvements",
+    "target_branch": null,
+    "source_commit": null
   }
 }
 EOF
+    } >> /tmp/x.sh.finalize.log
 }
 
 function tellName() {
